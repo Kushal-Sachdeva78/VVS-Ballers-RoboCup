87e29b6b88dcd6c325d3de87189a4cebcc48f963
Kushal Sachdeva <kushal.sach78@gmail.com>
2026-01-20 14:03:27 +0530

    Update RoboCup_Nationals_Kicker_Code.ino

--- RoboCup Nationals 2025-26/Code/RoboCup_Nationals_Kicker Code/RoboCup_Nationals_Kicker_Code.ino
+++ RoboCup Nationals 2025-26/Code/RoboCup_Nationals_Kicker Code/RoboCup_Nationals_Kicker_Code.ino
@@ -1,10 +1,3 @@
-/*
- * Ultrasonic Sensor (HC-SR04) + Relay Module
- * Trig: D2
- * Echo: D3
- * Relay: D7
- */
-
 const int trigPin = 2;
 const int echoPin = 3;
 const int relayPin = 21;
@@ -13,9 +6,6 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(relayPin, OUTPUT);
-
-  // Initialize relay to OFF 
-  // (Most relay modules are 'Active Low', so HIGH = OFF)
   digitalWrite(relayPin, HIGH); 
   
   Serial.begin(9600);
@@ -25,23 +15,17 @@ void loop() {
   long duration;
   int distance;
 
-  // Clear the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
-  // Trigger the sensor by sending a 10us pulse
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Read the echoPin (returns the sound travel time in microseconds)
   duration = pulseIn(echoPin, HIGH);
 
-  // Calculate distance in centimeters
   distance = duration * 0.034 / 2;
 
-
-  // Debugging output to Serial Monitor
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
